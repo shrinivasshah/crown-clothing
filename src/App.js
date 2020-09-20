f1fad19a60298d3ab55d569a5a18b79113111351
iamshaunjp <shrinivasshah05@gmail.com>
2020-09-20 13:16:22 +0530

    added crown logo styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from 'react'
 import Homepage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/Shop'
 import {Route, Switch} from 'react-router-dom'
+import Header from './components/header/Header'
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
             <Route exact path="/" component={Homepage}/>
             <Route path='/shop' component={ShopPage} />
